b19e9904e343964065fa31e74b6827a7695bc23c
charvi03 <charvikhurana01@gmail.com>
2023-06-30 19:29:37 +0530

    init

--- mainScript.js
+++ mainScript.js
@@ -21,27 +21,52 @@ var swiper = new Swiper(".home-slider", {
     clickable: true,
   },
 });
-var swiper = new Swiper(".blogs-slider", {
-  spaceBetween: 20,
-  grabCursor: true,
-  loop: true,
-  autoplay: {
-    delay: 7500,
-    disableOnInteraction: false,
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  //getPost()
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+const dailypost = document.querySelectorAll(".quote");
+const quoteTitle = document.querySelector("[data-quoteTitle]");
+const swiperContainer = document.querySelector(".blogs-slider");
+const createSlide = document.querySelector(".createSlide");
+
+const apiKey = "LTn5vNHSA0riVDyddRz5uA==jIbmWG57OnnvDyk4";
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey,
   },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    991: {
-      slidesPerView: 3,
-    },
-  },
-});
+};
+var category = "fitness";
+const apiUrl = "https://api.api-ninjas.com/v1/quotes?category=" + category;
+
+async function getPost() {
+  for (let index = 0; index < 5; index++) {
+    const response = await fetch(apiUrl, options);
+    var data = await response.json();
+    let text = data[0].quote;
+    dailypost[index].innerText = text;
+  }
+}
+
+getPost();
